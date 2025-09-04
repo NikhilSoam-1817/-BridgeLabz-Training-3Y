ace86cc3b011dc6d0ea813d3f4b5a9eb6918cd40
Nikihl_Soam <soamnikihl64@gmail.com>
2025-09-04 16:51:59 +0530

    MovieTicket adding

---
+++ 2-9-25/level2/MovieTicket.java
@@ -0,0 +1,22 @@
+public class MovieTicket {
+    private String movieName;
+    private String seatNumber;
+    private double price;
+
+    public MovieTicket(String movieName, String seatNumber, double price) {
+        this.movieName = movieName;
+        this.seatNumber = seatNumber;
+        this.price = price;
+    }
+
+    public void bookTicket(String seat, double ticketPrice) {
+        seatNumber = seat;
+        price = ticketPrice;
+    }
+
+    public void displayDetails() {
+        System.out.println("Movie: " + movieName);
+        System.out.println("Seat: " + seatNumber);
+        System.out.println("Price: " + price);
+    }
+}
