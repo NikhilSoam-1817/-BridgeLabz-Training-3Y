e6a56097a10a79120fb7244a4559447227bd4a2b
Nikihl_Soam <soamnikihl64@gmail.com>
2025-09-04 16:44:30 +0530

    MobilePhoneMain adding

---
+++ 2-9-25/level1/MobilePhoneMain.java
@@ -0,0 +1,6 @@
+public class MobilePhoneMain {
+    public static void main(String[] args) {
+        MobilePhone m1 = new MobilePhone("Samsung", "S24", 75000);
+        m1.displayDetails();
+    }
+}
