8c0f3a2bdc41fc1a7d585f497d643672491e2a16
Nikihl_Soam <soamnikihl64@gmail.com>
2025-09-16 17:05:24 +0530

    add animal class

---
+++ Inheritance/animal/Animal.java
@@ -0,0 +1,10 @@
+package animal;
+public class Animal {
+    protected String name;
+    protected int age;
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public void makeSound() {}
+}
