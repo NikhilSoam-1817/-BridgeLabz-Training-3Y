e1163b3421275477c37edc12d8d608f23699c80e
Nikihl_Soam <soamnikihl64@gmail.com>
2025-09-16 17:16:08 +0530

    add course

---
+++ Inheritance/course/Course.java
@@ -0,0 +1,9 @@
+package course;
+public class Course {
+    protected String courseName;
+    protected int duration;
+    public Course(String courseName, int duration) {
+        this.courseName = courseName;
+        this.duration = duration;
+    }
+}
