b0b3b58f2cb9c802a358470133303dd94c2feff7
Nikihl_Soam <soamnikihl64@gmail.com>
2025-09-16 17:06:14 +0530

    add animal class

---
+++ Inheritance/animal/Bird.java
@@ -0,0 +1,6 @@
+package animal;
+public class Bird extends Animal {
+    public Bird(String name, int age) { super(name, age); }
+    @Override
+    public void makeSound() { System.out.println("Chirp"); }
+}
