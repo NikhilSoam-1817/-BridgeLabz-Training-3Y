6b587bb326362f72665e11dc45845688467e261d
Nikihl_Soam <soamnikihl64@gmail.com>
2025-09-16 17:20:42 +0530

    add course

---
+++ Inheritance/course/PaidOnlineCourse.java
@@ -0,0 +1,10 @@
+package course;
+public class PaidOnlineCourse extends OnlineCourse {
+    private double fee;
+    private double discount;
+    public PaidOnlineCourse(String courseName, int duration, String platform, boolean isRecorded, double fee, double discount) {
+        super(courseName, duration, platform, isRecorded);
+        this.fee = fee;
+        this.discount = discount;
+    }
+}
