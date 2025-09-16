c9f6e2c8c6113c93d0c5017cc82cb972201fbf3a
Nikihl_Soam <soamnikihl64@gmail.com>
2025-09-16 17:25:29 +0530

    add in employee

---
+++ Inheritance/employee/Employee.java
@@ -0,0 +1,12 @@
+package employee;
+public class Employee {
+    protected String name;
+    protected int id;
+    protected double salary;
+    public Employee(String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+    public void displayDetails() {}
+}
